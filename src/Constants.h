5d2fd284fbd4db4b24d21b9751fba112181abe2f
v0ncent <83373495+v0ncent@users.noreply.github.com>
2025-09-29 01:19:49 -0500

    formatting and clean up

--- src/Constants.h
+++ src/Constants.h
@@ -1,8 +1,235 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
+// -------------------- Window Settings --------------------
 #define WINDOW_HEIGHT 1200
 #define WINDOW_WIDTH 1200
 #define WINDOW_TITLE "Learn Korean Hangul!"
 
+// -------------------- Hangul Image Filenames --------------------
+#define IMG_GIYEOK        "giyeok.PNG"
+#define IMG_SSANG_GIYEOK  "ssang-giyeok.PNG"
+#define IMG_NIEUN         "nieun.PNG"
+#define IMG_DIGEUT        "digeut.PNG"
+#define IMG_SSANG_DIGEUT  "ssang-digeut.PNG"
+#define IMG_RIEUL         "rieul.PNG"
+#define IMG_MIEUM         "mieum.PNG"
+#define IMG_BIEUP         "bieup.PNG"
+#define IMG_SSANG_BIEUP   "ssang-bieup.PNG"
+#define IMG_SIOT          "siot.PNG"
+#define IMG_SSANG_SIOT    "ssang-siot.PNG"
+#define IMG_IEUNG         "ieung.PNG"
+#define IMG_JIEUT         "jieut.PNG"
+#define IMG_SSANG_JIEUT   "ssang-jieut.PNG"
+#define IMG_CHIEUT        "chieut.PNG"
+#define IMG_KIEUK         "kieuk.PNG"
+#define IMG_TIEUT         "tieut.PNG"
+#define IMG_PIEUP         "pieup.PNG"
+#define IMG_HIEUT         "hieut.PNG"
+#define IMG_A             "a.PNG"
+#define IMG_EO            "eo.PNG"
+#define IMG_O             "o.PNG"
+#define IMG_U             "u.PNG"
+#define IMG_EU            "eu.PNG"
+#define IMG_I             "i.PNG"
+#define IMG_AE            "ae.PNG"
+#define IMG_E             "e.PNG"
+#define IMG_YA            "ya.PNG"
+#define IMG_YEO           "yeo.PNG"
+#define IMG_YO            "yo.PNG"
+#define IMG_YU            "yu.PNG"
+#define IMG_YAE           "yae.PNG"
+#define IMG_YE            "ye.PNG"
+#define IMG_WA            "wa.PNG"
+#define IMG_WAE           "wae.PNG"
+#define IMG_WO            "wo.PNG"
+#define IMG_WE            "we.PNG"
+#define IMG_OE            "oe.PNG"
+#define IMG_WI            "wi.PNG"
+#define IMG_UI            "ui.PNG"
+
+// -------------------- Romanizations & Pronunciations --------------------
+// Giyeok
+#define STR_GIYEOK        "giyok"
+#define PRON_GIYEOK_INIT  "k (walk)"
+#define PRON_GIYEOK_MED   "g (ghost)"
+#define PRON_GIYEOK_FINAL "k (walk)"
+
+// Ssang-giyeok
+#define STR_SSANG_GIYEOK  "ssang-giyeok"
+#define PRON_SSANG_GIYEOK "g (gone)"
+
+// Nieun
+#define STR_NIEUN         "nieun"
+#define PRON_NIEUN        "n (now)"
+
+// Digeut
+#define STR_DIGEUT        "digeut"
+#define PRON_DIGEUT_INIT  "t (talk)"
+#define PRON_DIGEUT_MED   "d (mind)"
+#define PRON_DIGEUT_FINAL "t (talk)"
+
+// Ssang-digeut
+#define STR_SSANG_DIGEUT  "ssang-digeut"
+#define PRON_SSANG_DIGEUT "d (dog)"
+
+// Rieul
+#define STR_RIEUL         "rieul"
+#define PRON_RIEUL_INIT   "r (run)"
+#define PRON_RIEUL_MED    "r (run)"
+#define PRON_RIEUL_FINAL  "l (real)"
+
+// Mieum
+#define STR_MIEUM         "mieum"
+#define PRON_MIEUM        "m (mop)"
+
+// Bieup
+#define STR_BIEUP         "bieup"
+#define PRON_BIEUP_INIT   "p (pool)"
+#define PRON_BIEUP_MED    "b (bay)"
+#define PRON_BIEUP_FINAL  "p (pool)"
+
+// Ssang-bieup
+#define STR_SSANG_BIEUP   "ssang-bieup"
+#define PRON_SSANG_BIEUP  "b (bird)"
+
+// Siot
+#define STR_SIOT          "siot"
+#define PRON_SIOT_INIT    "s (show)"
+#define PRON_SIOT_MED     "s (show)"
+#define PRON_SIOT_FINAL   "t (hot)"
+
+// Ssang-siot
+#define STR_SSANG_SIOT    "ssang-siot"
+#define PRON_SSANG_SIOT_INIT  "s (sun)"
+#define PRON_SSANG_SIOT_MED   "s (sun)"
+#define PRON_SSANG_SIOT_FINAL "t (hot)"
+
+// Ieung
+#define STR_IEUNG         "ieung"
+#define PRON_IEUNG_INIT   "silent"
+#define PRON_IEUNG_MED    "silent"
+#define PRON_IEUNG_FINAL  "ng (ring)"
+
+// Jieut
+#define STR_JIEUT         "jieut"
+#define PRON_JIEUT_INIT   "ch (chop)"
+#define PRON_JIEUT_MED    "j (jar)"
+#define PRON_JIEUT_FINAL  "t (hot)"
+
+// Ssang-jieut
+#define STR_SSANG_JIEUT   "ssang-jieut"
+#define PRON_SSANG_JIEUT_INIT  "j (jim)"
+#define PRON_SSANG_JIEUT_MED   "j (jim)"
+#define PRON_SSANG_JIEUT_FINAL "t (hot)"
+
+// Chieut
+#define STR_CHIEUT        "chieut"
+#define PRON_CHIEUT_INIT  "ch (itch)"
+#define PRON_CHIEUT_MED   "ch (itch)"
+#define PRON_CHIEUT_FINAL "t (hot)"
+
+// Kieuk
+#define STR_KIEUK         "kieuk"
+#define PRON_KIEUK        "kh (khaki)"
+
+// Tieut
+#define STR_TIEUT         "tieut"
+#define PRON_TIEUT        "t (tip)"
+
+// Pieup
+#define STR_PIEUP         "pieup"
+#define PRON_PIEUP        "p (pit)"
+
+// Hieut
+#define STR_HIEUT         "hieut"
+#define PRON_HIEUT_INIT   "h (hot)"
+#define PRON_HIEUT_MED    "h (hot)"
+#define PRON_HIEUT_FINAL  "silent"
+
+// A
+#define STR_A             "a"
+#define PRON_A            "ah (rah)"
+
+// Eo
+#define STR_EO            "eo"
+#define PRON_EO           "uh (run)"
+
+// O
+#define STR_O             "o"
+#define PRON_O            "oh (dough)"
+
+// U
+#define STR_U             "u"
+#define PRON_U            "oo (moon)"
+
+// Eu
+#define STR_EU            "eu"
+#define PRON_EU           "uh (brook)"
+
+// I
+#define STR_I             "i"
+#define PRON_I            "ee (meek)"
+
+// Ae
+#define STR_AE            "ae"
+#define PRON_AE           "ae (at)"
+
+// E
+#define STR_E             "e"
+#define PRON_E            "eh (met)"
+
+// Ya
+#define STR_YA            "ya"
+#define PRON_YA           "yah (yawn)"
+
+// Yeo
+#define STR_YEO           "yeo"
+#define PRON_YEO          "yuh (yum)"
+
+// Yo
+#define STR_YO            "yo"
+#define PRON_YO           "yoh (yodel)"
+
+// Yu
+#define STR_YU            "yu"
+#define PRON_YU           "yoo (view)"
+
+// Yae
+#define STR_YAE           "yae"
+#define PRON_YAE          "yae (yak)"
+
+// Ye
+#define STR_YE            "ye"
+#define PRON_YE           "yeh (yes)"
+
+// Wa
+#define STR_WA            "wa"
+#define PRON_WA           "wah (wand)"
+
+// Wae
+#define STR_WAE           "wae"
+#define PRON_WAE          "wae (wax)"
+
+// Wo
+#define STR_WO            "wo"
+#define PRON_WO           "wuh (wonder)"
+
+// We
+#define STR_WE            "we"
+#define PRON_WE           "weh (web)"
+
+// Oe
+#define STR_OE            "oe"
+#define PRON_OE           "way (wait)"
+
+// Wi
+#define STR_WI            "wi"
+#define PRON_WI           "wee (week)"
+
+// Ui
+#define STR_UI            "ui"
+#define PRON_UI           "uey (muey)"
+
 #endif
+
--- src/Util.cpp
+++ src/Util.cpp
@@ -5,6 +5,7 @@
 #include <stb_image.h>
 
 #include "Util.h"
+#include "Constants.h"
 
 Util::ImageData Util::loadImage(const std::string &path) {
     int w, h, ch;
@@ -29,457 +30,126 @@ std::vector<Util::Hangul> Util::loadHanguls() {
         return {};
     }
 
-    // this alphabet is so fucking long
-    hanguls.push_back(
-        {
-            loadImage(hangulPath.string() + "/" + "giyeok.PNG"),
-            "giyok",
-            "k (walk)",
-            "g (ghost)",
-            "k (walk)",
-            false
-        }
-        );
+    hanguls.push_back({ loadImage(hangulPath.string() + "/" + IMG_GIYEOK),
+                        STR_GIYEOK, PRON_GIYEOK_INIT, PRON_GIYEOK_MED, PRON_GIYEOK_FINAL, false });
 
-    hanguls.push_back(
-        {
-            loadImage(hangulPath.string() + "/" + "ssang-giyeok.PNG"),
-            "ssang-giyeok",
-            "g (gone)",
-            "g (gone)",
-            "g (gone)",
-            false
-        }
-        );
+    hanguls.push_back({ loadImage(hangulPath.string() + "/" + IMG_SSANG_GIYEOK),
+                        STR_SSANG_GIYEOK, PRON_SSANG_GIYEOK, PRON_SSANG_GIYEOK, PRON_SSANG_GIYEOK, false });
 
-    hanguls.push_back(
-        {
-            loadImage(hangulPath.string() + "/" + "nieun.PNG"),
-            "nieun",
-            "n (now)",
-            "n (now)",
-            "n (now)",
-            false
-        }
-        );
+    hanguls.push_back({ loadImage(hangulPath.string() + "/" + IMG_NIEUN),
+                        STR_NIEUN, PRON_NIEUN, PRON_NIEUN, PRON_NIEUN, false });
 
-    hanguls.push_back(
-        {
-            loadImage(hangulPath.string() + "/" + "digeut.PNG"),
-            "digeut",
-            "t (talk)",
-            "d (mind)",
-            "t (talk)",
-            false
-        }
-        );
+    hanguls.push_back({ loadImage(hangulPath.string() + "/" + IMG_DIGEUT),
+                        STR_DIGEUT, PRON_DIGEUT_INIT, PRON_DIGEUT_MED, PRON_DIGEUT_FINAL, false });
 
-    hanguls.push_back(
-        {
-            loadImage(hangulPath.string() + "/" + "ssang-digeut.PNG"),
-            "ssang-digeut",
-            "d (dog)",
-            "d (dog)",
-            "d (dog)",
-            false
-        }
-        );
+    hanguls.push_back({ loadImage(hangulPath.string() + "/" + IMG_SSANG_DIGEUT),
+                        STR_SSANG_DIGEUT, PRON_SSANG_DIGEUT, PRON_SSANG_DIGEUT, PRON_SSANG_DIGEUT, false });
 
-    hanguls.push_back(
-        {
-            loadImage(hangulPath.string() + "/" + "rieul.PNG"),
-            "rieul",
-            "r (run)",
-            "r (run)",
-            "l (real)",
-            false
-        }
-        );
+    hanguls.push_back({ loadImage(hangulPath.string() + "/" + IMG_RIEUL),
+                        STR_RIEUL, PRON_RIEUL_INIT, PRON_RIEUL_MED, PRON_RIEUL_FINAL, false });
 
-    hanguls.push_back(
-        {
-            loadImage(hangulPath.string() + "/" + "mieum.PNG"),
-            "mieum",
-            "m (mop)",
-            "m (mop)",
-            "m (mop)",
-            false
-        }
-        );
+    hanguls.push_back({ loadImage(hangulPath.string() + "/" + IMG_MIEUM),
+                        STR_MIEUM, PRON_MIEUM, PRON_MIEUM, PRON_MIEUM, false });
 
-    hanguls.push_back(
-        {
-            loadImage(hangulPath.string() + "/" + "bieup.PNG"),
-            "bieup",
-            "p (pool)",
-            "b (bay)",
-            "p (pool)",
-            false
-        }
-        );
+    hanguls.push_back({ loadImage(hangulPath.string() + "/" + IMG_BIEUP),
+                        STR_BIEUP, PRON_BIEUP_INIT, PRON_BIEUP_MED, PRON_BIEUP_FINAL, false });
 
-    hanguls.push_back(
-        {
-            loadImage(hangulPath.string() + "/" + "ssang-bieup.PNG"),
-            "ssang-bieup",
-            "b (bird)",
-            "b (bird)",
-            "b (bird)",
-            false
-        }
-        );
+    hanguls.push_back({ loadImage(hangulPath.string() + "/" + IMG_SSANG_BIEUP),
+                        STR_SSANG_BIEUP, PRON_SSANG_BIEUP, PRON_SSANG_BIEUP, PRON_SSANG_BIEUP, false });
 
-    hanguls.push_back(
-        {
-            loadImage(hangulPath.string() + "/" + "siot.PNG"),
-            "siot",
-            "s (show)",
-            "s (show)",
-            "t (hot)",
-            false
-        }
-        );
+    hanguls.push_back({ loadImage(hangulPath.string() + "/" + IMG_SIOT),
+                        STR_SIOT, PRON_SIOT_INIT, PRON_SIOT_MED, PRON_SIOT_FINAL, false });
 
-    hanguls.push_back(
-        {
-            loadImage(hangulPath.string() + "/" + "ssang-siot.PNG"),
-            "ssang-siot",
-            "s (sun)",
-            "s (sun)",
-            "t (hot)",
-            false
-        }
-        );
+    hanguls.push_back({ loadImage(hangulPath.string() + "/" + IMG_SSANG_SIOT),
+                        STR_SSANG_SIOT, PRON_SSANG_SIOT_INIT, PRON_SSANG_SIOT_MED, PRON_SSANG_SIOT_FINAL, false });
 
-    hanguls.push_back(
-        {
-            loadImage(hangulPath.string() + "/" + "ieung.PNG"),
-            "ieung",
-            "silent",
-            "silent",
-            "ng (ring)",
-            false
-        }
-        );
+    hanguls.push_back({ loadImage(hangulPath.string() + "/" + IMG_IEUNG),
+                        STR_IEUNG, PRON_IEUNG_INIT, PRON_IEUNG_MED, PRON_IEUNG_FINAL, false });
 
-    hanguls.push_back(
-        {
-            loadImage(hangulPath.string() + "/" + "jieut.PNG"),
-            "jieut",
-            "ch (chop)",
-            "j (jar)",
-            "t (hot)",
-            false
-        }
-        );
+    hanguls.push_back({ loadImage(hangulPath.string() + "/" + IMG_JIEUT),
+                        STR_JIEUT, PRON_JIEUT_INIT, PRON_JIEUT_MED, PRON_JIEUT_FINAL, false });
 
-    hanguls.push_back(
-        {
-            loadImage(hangulPath.string() + "/" + "ssang-jieut.PNG"),
-            "ssang-jieut",
-            "j (jim)",
-            "j (jim)",
-            "t (hot)",
-            false
-        }
-        );
+    hanguls.push_back({ loadImage(hangulPath.string() + "/" + IMG_SSANG_JIEUT),
+                        STR_SSANG_JIEUT, PRON_SSANG_JIEUT_INIT, PRON_SSANG_JIEUT_MED, PRON_SSANG_JIEUT_FINAL, false });
 
-    hanguls.push_back(
-        {
-            loadImage(hangulPath.string() + "/" + "chieut.PNG"),
-            "chieut",
-            "ch (itch)",
-            "ch (itch)",
-            "t (hot)",
-            false
-        }
-        );
+    hanguls.push_back({ loadImage(hangulPath.string() + "/" + IMG_CHIEUT),
+                        STR_CHIEUT, PRON_CHIEUT_INIT, PRON_CHIEUT_MED, PRON_CHIEUT_FINAL, false });
 
-    hanguls.push_back(
-        {
-            loadImage(hangulPath.string() + "/" + "kieuk.PNG"),
-            "kieuk",
-            "kh (khaki)",
-            "kh (khaki)",
-            "kh (khaki)",
-            false
-        }
-        );
+    hanguls.push_back({ loadImage(hangulPath.string() + "/" + IMG_KIEUK),
+                        STR_KIEUK, PRON_KIEUK, PRON_KIEUK, PRON_KIEUK, false });
 
-    hanguls.push_back(
-        {
-            loadImage(hangulPath.string() + "/" + "tieut.PNG"),
-            "tieut",
-            "t (tip)",
-            "t (tip)",
-            "t (tip)",
-            false
-        }
-        );
+    hanguls.push_back({ loadImage(hangulPath.string() + "/" + IMG_TIEUT),
+                        STR_TIEUT, PRON_TIEUT, PRON_TIEUT, PRON_TIEUT, false });
 
-    hanguls.push_back(
-    {
-        loadImage(hangulPath.string() + "/" + "pieup.PNG"),
-        "pieup",
-        "p (pit)",
-        "p (pit)",
-        "p (pit)",
-        false
-    }
-    );
+    hanguls.push_back({ loadImage(hangulPath.string() + "/" + IMG_PIEUP),
+                        STR_PIEUP, PRON_PIEUP, PRON_PIEUP, PRON_PIEUP, false });
 
-    hanguls.push_back(
-        {
-        loadImage(hangulPath.string() + "/" + "hieut.PNG"),
-        "hieut",
-        "h (hot)",
-        "h (hot)",
-        "silent",
-        false
-    }
-    );
+    hanguls.push_back({ loadImage(hangulPath.string() + "/" + IMG_HIEUT),
+                        STR_HIEUT, PRON_HIEUT_INIT, PRON_HIEUT_MED, PRON_HIEUT_FINAL, false });
 
-    hanguls.push_back(
-{
-    loadImage(hangulPath.string() + "/" + "a.PNG"),
-    "a",
-    "ah (rah)",
-    "ah (rah)",
-    "ah (rah)",
-    false
-    }
-    );
+    hanguls.push_back({ loadImage(hangulPath.string() + "/" + IMG_A),
+                        STR_A, PRON_A, PRON_A, PRON_A, false });
 
-    hanguls.push_back(
-{
-    loadImage(hangulPath.string() + "/" + "eo.PNG"),
-    "eo",
-    "uh (run)",
-    "uh (run)",
-    "uh (run)",
-    false
-    }
-    );
+    hanguls.push_back({ loadImage(hangulPath.string() + "/" + IMG_EO),
+                        STR_EO, PRON_EO, PRON_EO, PRON_EO, false });
 
-    hanguls.push_back(
-{
-    loadImage(hangulPath.string() + "/" + "o.PNG"),
-    "o",
-    "oh (dough)",
-    "oh (dough)",
-    "oh (dough)",
-    false
-    }
-    );
+    hanguls.push_back({ loadImage(hangulPath.string() + "/" + IMG_O),
+                        STR_O, PRON_O, PRON_O, PRON_O, false });
 
-    hanguls.push_back(
-    {
-    loadImage(hangulPath.string() + "/" + "u.PNG"),
-    "u",
-    "oo (moon)",
-    "oo (moon)",
-    "oo (moon)",
-    false
-    }
-    );
+    hanguls.push_back({ loadImage(hangulPath.string() + "/" + IMG_U),
+                        STR_U, PRON_U, PRON_U, PRON_U, false });
 
-    hanguls.push_back(
-    {
-    loadImage(hangulPath.string() + "/" + "eu.PNG"),
-    "eu",
-    "uh (brook)",
-    "uh (brook)",
-    "uh (brook)",
-    false
-    }
-    );
+    hanguls.push_back({ loadImage(hangulPath.string() + "/" + IMG_EU),
+                        STR_EU, PRON_EU, PRON_EU, PRON_EU, false });
 
-    hanguls.push_back(
-    {
-    loadImage(hangulPath.string() + "/" + "i.PNG"),
-    "i",
-    "ee (meek)",
-    "ee (meek)",
-    "ee (meek)",
-    false
-    }
-    );
+    hanguls.push_back({ loadImage(hangulPath.string() + "/" + IMG_I),
+                        STR_I, PRON_I, PRON_I, PRON_I, false });
 
-    hanguls.push_back(
-    {
-    loadImage(hangulPath.string() + "/" + "ae.PNG"),
-    "ae",
-    "ae (at)",
-    "ae (at)",
-    "ae (at)",
-    false
-    }
-    );
+    hanguls.push_back({ loadImage(hangulPath.string() + "/" + IMG_AE),
+                        STR_AE, PRON_AE, PRON_AE, PRON_AE, false });
 
-    hanguls.push_back(
-    {
-    loadImage(hangulPath.string() + "/" + "e.PNG"),
-    "e",
-    "eh (met)",
-    "eh (met)",
-    "eh (met)",
-    false
-    }
-    );
+    hanguls.push_back({ loadImage(hangulPath.string() + "/" + IMG_E),
+                        STR_E, PRON_E, PRON_E, PRON_E, false });
 
-    hanguls.push_back(
-    {
-    loadImage(hangulPath.string() + "/" + "ya.PNG"),
-    "ya",
-    "yah (yawn)",
-    "yah (yawn)",
-    "yah (yawn)",
-    false
-    }
-    );
+    hanguls.push_back({ loadImage(hangulPath.string() + "/" + IMG_YA),
+                        STR_YA, PRON_YA, PRON_YA, PRON_YA, false });
 
-    hanguls.push_back(
-    {
-    loadImage(hangulPath.string() + "/" + "e.PNG"),
-    "e",
-    "eh (met)",
-    "eh (met)",
-    "eh (met)",
-    false
-    }
-    );
+    hanguls.push_back({ loadImage(hangulPath.string() + "/" + IMG_YEO),
+                        STR_YEO, PRON_YEO, PRON_YEO, PRON_YEO, false });
 
-    hanguls.push_back(
-    {
-    loadImage(hangulPath.string() + "/" + "yeo.PNG"),
-    "yeo",
-    "yuh (yum)",
-    "yuh (yum)",
-    "yuh (yum)",
-    false
-    }
-    );
+    hanguls.push_back({ loadImage(hangulPath.string() + "/" + IMG_YO),
+                        STR_YO, PRON_YO, PRON_YO, PRON_YO, false });
 
-    hanguls.push_back(
-    {
-    loadImage(hangulPath.string() + "/" + "yo.PNG"),
-    "yo",
-    "yoh (yodel)",
-    "yoh (yodel)",
-    "yoh (yodel)",
-    false
-    }
-    );
+    hanguls.push_back({ loadImage(hangulPath.string() + "/" + IMG_YU),
+                        STR_YU, PRON_YU, PRON_YU, PRON_YU, false });
 
-    hanguls.push_back(
-    {
-    loadImage(hangulPath.string() + "/" + "yu.PNG"),
-    "yu",
-    "yoo (view)",
-    "yoo (view)",
-    "yoo (view)",
-    false
-    }
-    );
+    hanguls.push_back({ loadImage(hangulPath.string() + "/" + IMG_YAE),
+                        STR_YAE, PRON_YAE, PRON_YAE, PRON_YAE, false });
 
-    hanguls.push_back(
-    {
-    loadImage(hangulPath.string() + "/" + "yae.PNG"),
-    "yae",
-    "yae (yak)",
-    "yae (yak)",
-    "yae (yak)",
-    false
-    }
-    );
+    hanguls.push_back({ loadImage(hangulPath.string() + "/" + IMG_YE),
+                        STR_YE, PRON_YE, PRON_YE, PRON_YE, false });
 
-    hanguls.push_back(
-    {
-    loadImage(hangulPath.string() + "/" + "ye.PNG"),
-    "ye",
-    "yeh (yes)",
-    "yeh (yes)",
-    "yeh (yes)",
-    false
-    }
-    );
+    hanguls.push_back({ loadImage(hangulPath.string() + "/" + IMG_WA),
+                        STR_WA, PRON_WA, PRON_WA, PRON_WA, false });
 
-    hanguls.push_back(
-    {
-    loadImage(hangulPath.string() + "/" + "wa.PNG"),
-    "wa",
-    "wah (wand)",
-    "wah (wand)",
-    "wah (wand)",
-    false
-    }
-    );
+    hanguls.push_back({ loadImage(hangulPath.string() + "/" + IMG_WAE),
+                        STR_WAE, PRON_WAE, PRON_WAE, PRON_WAE, false });
 
-    hanguls.push_back(
-    {
-    loadImage(hangulPath.string() + "/" + "wae.PNG"),
-    "wae",
-    "wae (wax)",
-    "wae (wax)",
-    "wae (wax)",
-    false
-    }
-    );
+    hanguls.push_back({ loadImage(hangulPath.string() + "/" + IMG_WO),
+                        STR_WO, PRON_WO, PRON_WO, PRON_WO, false });
 
-    hanguls.push_back(
-    {
-    loadImage(hangulPath.string() + "/" + "wo.PNG"),
-    "wo",
-    "wuh (wonder)",
-    "wuh (wonder)",
-    "wuh (wonder)",
-    false
-    }
-    );
+    hanguls.push_back({ loadImage(hangulPath.string() + "/" + IMG_WE),
+                        STR_WE, PRON_WE, PRON_WE, PRON_WE, false });
 
-    hanguls.push_back(
-    {
-    loadImage(hangulPath.string() + "/" + "we.PNG"),
-    "we",
-    "weh (web)",
-    "weh (web)",
-    "weh (web)",
-    false
-    }
-    );
+    hanguls.push_back({ loadImage(hangulPath.string() + "/" + IMG_OE),
+                        STR_OE, PRON_OE, PRON_OE, PRON_OE, false });
 
-    hanguls.push_back(
-    {
-    loadImage(hangulPath.string() + "/" + "oe.PNG"),
-    "oe",
-    "way (wait)",
-    "way (wait)",
-    "way (wait)",
-    false
-    }
-    );
+    hanguls.push_back({ loadImage(hangulPath.string() + "/" + IMG_WI),
+                        STR_WI, PRON_WI, PRON_WI, PRON_WI, false });
 
-    hanguls.push_back(
-    {
-    loadImage(hangulPath.string() + "/" + "wi.PNG"),
-    "wi",
-    "wee (week)",
-    "wee (week)",
-    "wee (week)",
-    false
-    }
-    );
-
-    hanguls.push_back(
-    {
-    loadImage(hangulPath.string() + "/" + "ui.PNG"),
-    "ui",
-    "uey (muey)",
-    "uey (muey)",
-    "uey (muey)",
-    false
-    }
-    );
+    hanguls.push_back({ loadImage(hangulPath.string() + "/" + IMG_UI),
+                        STR_UI, PRON_UI, PRON_UI, PRON_UI, false });
 
     return hanguls;
 }
+
